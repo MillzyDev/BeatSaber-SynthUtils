751235507cabc64f9147725dbe19cbdda2f35fc2
Millzy <millzyyt08@gmail.com>
2022-01-03 16:40:08 +0000

    sync to redbar

--- include/Components/RedbarSync.hpp
+++ include/Components/RedbarSync.hpp
@@ -6,8 +6,12 @@ using namespace UnityEngine;
 #include "HMUI/ImageView.hpp"
 using namespace HMUI;
 
+#include "TMPro/TextMeshProUGUI.hpp"
+using namespace TMPro;
+
 DECLARE_CLASS_CODEGEN(SynthUtils::Components, RedbarSync, MonoBehaviour,
     DECLARE_INSTANCE_FIELD(ImageView *, energyBar);
+    DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, percentage);
 
     DECLARE_INSTANCE_METHOD(void, Start);
     DECLARE_INSTANCE_METHOD(void, Update);
