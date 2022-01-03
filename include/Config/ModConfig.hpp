8cd46148bd20f8f3bed45817ec184940f4635aa4
Millzy <millzyyt08@gmail.com>
2022-01-03 15:20:47 +0000

    corrected this

--- include/Config/ModConfig.hpp
+++ include/Config/ModConfig.hpp
@@ -6,17 +6,17 @@
 using namespace UnityEngine;
 
 DECLARE_CONFIG(ModConfig,
-    CONFIG_VALUE(sp_enabled, bool, "ScorePercentageEnabled", false);
-    CONFIG_VALUE(sp_redbarSync, bool, "ScorePercentageSyncToRedbar", false);
-    CONFIG_VALUE(sp_fontSize, float, "ScorePercentageFontSize", 15.0f);
-    CONFIG_VALUE(sp_positionOffset, Vector2, "ScorePercentagePositionOffset", Vector2(0.0f, -0.3f));
-
-    CONFIG_VALUE(vm_enabled, bool, "VisualModeEnabled", false);
+    CONFIG_VALUE(ep_enabled, bool, "EnergyPercentageEnabled", true);
+    CONFIG_VALUE(ep_redbarSync, bool, "EnergyPercentageSyncToRedbar", false);
+    CONFIG_VALUE(ep_fontSize, float, "EnergyPercentageFontSize", 15.0f);
+    CONFIG_VALUE(ep_positionOffset, Vector2, "EnergyPercentagePositionOffset", Vector2(0.0f, -0.4f));
+    CONFIG_VALUE(ep_decimalPlaces, int, "EnergyPercentageDecimalPlaces", 0);
 
     CONFIG_INIT_FUNCTION(
-        CONFIG_INIT_VALUE(sp_enabled);
-        CONFIG_INIT_VALUE(sp_redbarSync);
-        CONFIG_INIT_VALUE(sp_fontSize);
-        CONFIG_INIT_VALUE(sp_positionOffset);
+        CONFIG_INIT_VALUE(ep_enabled);
+        CONFIG_INIT_VALUE(ep_redbarSync);
+        CONFIG_INIT_VALUE(ep_fontSize);
+        CONFIG_INIT_VALUE(ep_positionOffset);
+        CONFIG_INIT_VALUE(ep_decimalPlaces);
     )
 )
