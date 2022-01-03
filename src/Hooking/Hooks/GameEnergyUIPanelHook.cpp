e72072b9e8b9c2a16ec20e4fce06a1a33fad9f5e
Millzy <millzyyt08@gmail.com>
2022-01-03 15:20:57 +0000

    fixed crashes

--- src/Hooking/Hooks/GameEnergyUIPanelHook.cpp
+++ src/Hooking/Hooks/GameEnergyUIPanelHook.cpp
@@ -1,7 +1,6 @@
 #include "main.hpp"
 
-#include "Hooking/Hooks/GameEnergyUIPanelHook.hpp"
-using namespace SynthUtils::Hooking::Hooks;
+#include "Hooking/SUHooks.hpp"
 
 #include "GlobalNamespace/GameEnergyUIPanel.hpp"
 using namespace GlobalNamespace;
@@ -12,44 +11,63 @@ using namespace QuestUI::BeatSaberUI;
 #include "UnityEngine/Vector3.hpp"
 using namespace UnityEngine;
 
+#include "TMPro/TextAlignmentOptions.hpp"
+#include "TMPro/TextMeshProUGUI.hpp"
+using namespace TMPro;
+
+GameObject *percentageCanvas;
+TextMeshProUGUI *percentage;
+
+std::string baseFormat = "%%.%if%%%%";
+std::string finalFormat;
+
 MAKE_HOOK_MATCH(GameEnergyUIPanel_Start, &GameEnergyUIPanel::Start, void,
     GameEnergyUIPanel *self
 ) {
     GameEnergyUIPanel_Start(self);
 
-     if (getModConfig().sp_enabled.GetValue()) {
-        getLogger().info("Creating EnergyPercentage Canvas...");
+    getLogger().info("%s", to_utf8(csstrtostr(self->energyBar->get_gameObject()->get_name())).c_str());
 
-        GameObject *percentageCanvas = GameEnergyUIPanelHook::percentageCanvas;
+     if (getModConfig().ep_enabled.GetValue()) {
+         getLogger().info("Creating format");
+         finalFormat = string_format(baseFormat.c_str(), getModConfig().ep_decimalPlaces.GetValue());
+
+        getLogger().info("Creating EnergyPercentage canvas...");
+
         percentageCanvas = CreateCanvas();
+
+        getLogger().info("Applying position offset to EnergyPercentage canvas...");
         auto pos = self->get_transform()->get_position();
+
+        pos.x += getModConfig().ep_positionOffset.GetValue().x;
+        pos.y += getModConfig().ep_positionOffset.GetValue().y;
+
         percentageCanvas->get_transform()->set_position(pos);
-        pos = percentageCanvas->get_transform()->get_position();
-        pos.x += getModConfig().sp_positionOffset.GetValue().x;
-        pos.y += getModConfig().sp_positionOffset.GetValue().y;
-        percentageCanvas->get_transform()->set_position(pos);
-        GameEnergyUIPanelHook::percentage = CreateText(percentageCanvas->get_transform(), "50%");
-        GameEnergyUIPanelHook::percentage->set_fontSize(getModConfig().sp_fontSize.GetValue());
-        GameEnergyUIPanelHook::percentage->set_alignment(TextAlignmentOptions::Center);
+
+        getLogger().info("Creating Percentage text...");
+        percentage = CreateText(percentageCanvas->get_transform(), string_format(finalFormat.c_str(), 50.0f));
+        percentage->set_alignment(TextAlignmentOptions::Center);
+
+        getLogger().info("Setting fontSize...");
+        percentage->set_fontSize(getModConfig().ep_fontSize.GetValue());
+        getLogger().info("Finished creating EnergyPercentage");
     }
 }
 
 MAKE_HOOK_MATCH(GameEnergyUIPanel_HandleGameEnergyDidChange, &GameEnergyUIPanel::HandleGameEnergyDidChange, void,
     GameEnergyUIPanel *self, float energy
 ) {
-    getLogger().info("Game energy changed to %.0f%%", energy * 100);
+    getLogger().info("Game energy changed to %f", energy);
 
-    GameEnergyUIPanelHook::percentage->set_text(il2cpp_utils::newcsstr(string_format("%.0f%%", energy * 100)));
+    if (percentage != nullptr)
+        percentage->set_text(il2cpp_utils::newcsstr(string_format(finalFormat.c_str(), energy * 100)));
 
     GameEnergyUIPanel_HandleGameEnergyDidChange(self, energy);
 }
 
-GameObject *GameEnergyUIPanelHook::percentageCanvas = nullptr;
-TextMeshProUGUI *GameEnergyUIPanelHook::percentage = nullptr;
-
-void GameEnergyUIPanelHook::installHooks() {
-    INSTALL_HOOK(getLogger(), GameEnergyUIPanel_Start);
-    INSTALL_HOOK(getLogger(), GameEnergyUIPanel_HandleGameEnergyDidChange);
+void GameEnergyUIPanelHook(Logger &logger) {
+    INSTALL_HOOK(logger, GameEnergyUIPanel_Start);
+    INSTALL_HOOK(logger, GameEnergyUIPanel_HandleGameEnergyDidChange);
 }
 
-std::string GameEnergyUIPanelHook::getName() { return "GameEnergyUIPanel"; }
+InstallHooks(GameEnergyUIPanelHook);
